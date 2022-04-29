e222e7edcf564d59ed3a1bf363af79a49948a86a
ivanalexisc <iac_1999@hotmail.com>
2022-04-29 02:24:05 -0300

    subi el proyecto

---
+++ src/controllers/productsController.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = {
+	// Root - Show all products
+	index: (req, res) => {
+		res.render('products',{products})
+	},
+
+	// Detail - Detail from one product
+	detail: (req, res) => {
+		for(let i=0; i<products.length;i++){
+			if(products[i].id == req.params.id)
+			res.render('detail',{producto:products[i],toThousand})
+		}
+	},
+
+	// Create - Form to create
+	create: (req, res) => {
+		res.render("product-create-form")
+	},
+	
+	// Create -  Method to store
+	store: (req, res) => {
+		// Do the magic
+	},
+
+	// Update - Form to edit
+	edit: (req, res) => {
+	let editProduct = products.find(producto => producto.id==req.params.id);
+	res.render('product-edit-form', {'editProduct': editProduct})	
+		
+	},
+	// Update - Method to update
+	update: (req, res) => {
+		// Do the magic
+	},
+
+	// Delete - Delete one product from DB
+	destroy : (req, res) => {
+		// Do the magic
+	}
+};
+
+module.exports = controller;
