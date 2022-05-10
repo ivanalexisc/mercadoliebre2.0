9dc12987412c5ac261f2dcb8f0bfb431854b3eb4
ivanalexisc <iac_1999@hotmail.com>
2022-05-10 01:42:17 -0300

    modifique

--- src/routes/products.js
+++ src/routes/products.js
@@ -17,7 +17,20 @@ var storage = multer.diskStorage({
     }
   });
    
-  var upload = multer({ storage: storage });
+  var upload = multer({ 
+      storage,
+
+      //VALIDATE IMAGE
+      fileFilter: (req,file,cb) => {
+          const acceptedExtensions = ['.jpg', '.jpeg', '.png'];
+          const ext = path.extname(file.originalname);
+          if(!acceptedExtensions.includes(ext)){
+              req.file = file;
+          }
+          cb(null, acceptedExtensions.includes(ext));
+      }
+
+    });
 
 /*** GET ALL PRODUCTS ***/ 
 router.get('/', productsController.index); 
